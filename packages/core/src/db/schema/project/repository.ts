43348d53db35de60f5880a92a789213855637175
Hernan Acero @acerohernan <contacto.acero.hernan@gmail.com>
2023-09-26 09:16:27 -0500

    build(core): Update project repository schema

--- packages/core/src/db/schema/project/repository.ts
+++ packages/core/src/db/schema/project/repository.ts
@@ -1,18 +1,25 @@
 import { relations } from "drizzle-orm";
-import { pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+import { pgTable, uuid, varchar, index } from "drizzle-orm/pg-core";
 
 import { primaryKeys } from "../../utils";
 
 import { projects } from "./index";
 
-export const projectRepositories = pgTable("project_repositories", {
-  ...primaryKeys,
-  projectId: uuid("project_id").unique().notNull(),
-  url: varchar("url").notNull(),
-  name: varchar("name").notNull(),
-  namespace: varchar("namespace").notNull(),
-  branch: varchar("branch").notNull(),
-});
+export const projectRepositories = pgTable(
+  "project_repositories",
+  {
+    ...primaryKeys,
+    projectId: uuid("project_id").unique().notNull(),
+    url: varchar("url").notNull(),
+    name: varchar("name").notNull(),
+    owner: varchar("owner").notNull(),
+    namespace: varchar("namespace").notNull(), // @deprecated
+    branch: varchar("branch").notNull(), // @deprecated
+  },
+  (table) => ({
+    nameAndOwnerIdx: index("name_and_owner_idx").on(table.name, table.owner),
+  })
+);
 
 export const projectRepositoriesRelations = relations(
   projectRepositories,
